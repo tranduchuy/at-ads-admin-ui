410fc32e5057f601b39e353b0e29075d1ed07b40
HuyTD <vincenthuytd@gmail.com>
2019-11-01 06:31:36 +0700

    Build 1-11-2019

---
+++ build/precache-manifest.7c9c53054cb100d91cf96e00fb35bc57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad5ad5c4fc60b3ff53ee15a76a95996",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fda84acfb4edf8bdddb",
+    "url": "/static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "a712255c2a6b23c4dc4e",
+    "url": "/static/css/main.f9b7fc2b.chunk.css"
+  },
+  {
+    "revision": "4fda84acfb4edf8bdddb",
+    "url": "/static/js/2.e45eda1b.chunk.js"
+  },
+  {
+    "revision": "a712255c2a6b23c4dc4e",
+    "url": "/static/js/main.fe23e1d1.chunk.js"
+  },
+  {
+    "revision": "4c256657c299b03a2570",
+    "url": "/static/js/runtime~main.e2135336.js"
+  },
+  {
+    "revision": "ee5470094be0411c9072f397d541696b",
+    "url": "/static/media/app-logo.ee547009.png"
+  },
+  {
+    "revision": "d914334bfb315da0efe344d78c78b40c",
+    "url": "/static/media/dark-material-bg.d914334b.jpg"
+  }
+]);
