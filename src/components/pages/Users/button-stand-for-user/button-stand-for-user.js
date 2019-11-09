e6476f44fc069fa6391470d8c51a806d4a16c0db
HuyTD <vincenthuytd@gmail.com>
2019-11-09 15:53:25 +0700

    Fix bug cannot save info target user

--- src/components/pages/Users/button-stand-for-user/button-stand-for-user.js
+++ src/components/pages/Users/button-stand-for-user/button-stand-for-user.js
@@ -1,69 +1,93 @@
 import React from 'react';
 import { withCookies } from 'react-cookie';
-import * as  PropTypes from 'prop-types';
+import * as PropTypes from 'prop-types';
 import { Icon, Tooltip, Button } from 'antd';
 import { connect } from 'react-redux';
 import * as actions from '../../../../actions';
 import secret from '../../../../config/secret';
 import { COOKIE_NAMES } from '../../../../constants/cookie-names';
 import { UserRoles } from '../../../../constants/user-role';
+import CookieService from '../../../../services/cookie.service';
+
+const fieldsOfTargetUser = [
+  '_id',
+  'avatar',
+  'createdAt',
+  'email',
+  'name',
+  'phone',
+  'role',
+  'googleId'
+];
 
 class ButtonStandForUser extends React.Component {
+  secret = secret;
 
-	secret = secret;
+  constructor(props) {
+    super(props);
 
-	constructor(props) {
-		super(props);
+    this.onClick = this.onClick.bind(this);
+  }
 
-		this.onClick = this.onClick.bind(this);
-	}
+  onClick() {
+    this.props.setAppLoading(true);
+    const token = CookieService.get(COOKIE_NAMES.token);
+    const loggedInUser = CookieService.get(COOKIE_NAMES.user);
+    const targetUser = JSON.parse(JSON.stringify(this.props.user));
+    targetUser._id = targetUser.id;
 
-	onClick() {
-		this.props.setAppLoading(true);
-		const { cookies } = this.props;
-		const token = cookies.get(COOKIE_NAMES.token);
-		const loggedInUser = cookies.get(COOKIE_NAMES.user);
-		const targetUser = JSON.parse(JSON.stringify(this.props.user));
-		targetUser._id = targetUser.id;
-		console.log(secret);
-		cookies.set(COOKIE_NAMES.FRONT_END.user, targetUser, {path: '/', domain: secret.MAIN_DOMAIN}); // target user info
-		cookies.set(COOKIE_NAMES.FRONT_END.standBy, token, {path: '/', domain: secret.MAIN_DOMAIN}); // Admin info
-		cookies.set(COOKIE_NAMES.FRONT_END.token, loggedInUser, {path: '/', domain: secret.MAIN_DOMAIN}); // admin token
+    const userInfoToBeSaved = {};
+    fieldsOfTargetUser.forEach(f => userInfoToBeSaved[f] = targetUser[f]);
 
-		// console.log('value: ', this.secret);
-		setTimeout(() => {
-			window.open(this.secret.FRONT_END_DOMAIN, '_blank');
-			this.props.setAppLoading(false);
-		}, 1000);
-	}
+    CookieService.set(COOKIE_NAMES.FRONT_END.user, JSON.stringify(userInfoToBeSaved), {
+      path: '/',
+      domain: secret.MAIN_DOMAIN
+    }); // target user info
+    CookieService.set(COOKIE_NAMES.FRONT_END.standBy, loggedInUser, {
+      path: '/',
+      domain: secret.MAIN_DOMAIN
+    }); // Admin info
+    CookieService.set(COOKIE_NAMES.FRONT_END.token, token, {
+      path: '/',
+      domain: secret.MAIN_DOMAIN
+    }); // admin token
 
-	render() {
-		if (this.props.user.role === UserRoles.endUser) {
-			return (
-				<Tooltip title='Thay quyền'>
-					<Button size='small'
-									onClick={this.onClick}>
-						<Icon type="login"/>
-					</Button>
-				</Tooltip>
-			);
-		}
+    // console.log('value: ', this.secret);
+    setTimeout(() => {
+      window.open(this.secret.FRONT_END_DOMAIN, '_blank');
+      this.props.setAppLoading(false);
+    }, 1000);
+  }
 
-		return <></>;
-	}
+  render() {
+    if (this.props.user.role === UserRoles.endUser) {
+      return (
+        <Tooltip title='Thay quyền'>
+          <Button size='small' onClick={this.onClick}>
+            <Icon type='login' />
+          </Button>
+        </Tooltip>
+      );
+    }
+
+    return <></>;
+  }
 }
 
 ButtonStandForUser.propTypes = {
-	users: PropTypes.object,
-	user: PropTypes.shape({
-		id: PropTypes.string.isRequired,
-		role: PropTypes.number.isRequired
-	}),
-	setAppLoading: PropTypes.func
+  users: PropTypes.object,
+  user: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    role: PropTypes.number.isRequired
+  }),
+  setAppLoading: PropTypes.func
 };
 
-const mapStateToProps = (state) => ({
-	users: state.users
+const mapStateToProps = state => ({
+  users: state.users
 });
 
-export default connect(mapStateToProps, actions)(withCookies(ButtonStandForUser));
+export default connect(
+  mapStateToProps,
+  actions
+)(ButtonStandForUser);
