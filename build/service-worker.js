93d8137602a884ddc2d67ab87cf59166c1213926
Tue Vo <vothaiminhtue0312@gmail.com>
2020-02-11 14:30:13 +0700

    build staging 11/2/2020

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5fdc40027d1b2a0ae051568b12bd3b79.js"
+  "/precache-manifest.12ca06b1e27278716404b0c6fb150b9e.js"
 );
 
 self.addEventListener('message', (event) => {
